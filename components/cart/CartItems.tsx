847c8e8f8f2fd797230bc42dea0cf15bb70320c8
Vedant Nimbarte <nimbartevedant@gmail.com>
2023-02-08 23:08:19 +0530

    Updated currency symbol in ccart

--- components/cart/CartItems.tsx
+++ components/cart/CartItems.tsx
@@ -1,16 +1,24 @@
-import {ICartItem} from 'boundless-api-client';
-import {Dispatch, SetStateAction, useEffect, useMemo, useRef, useState} from 'react';
-import {useAppDispatch, useAppSelector} from '../../hooks/redux';
-import {apiClient} from '../../lib/api';
-import {addPromise} from '../../redux/reducers/xhr';
-import {RootState} from '../../redux/store';
-import debounce from 'lodash/debounce';
-import CartRow from './CartRow';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faShoppingCart} from '@fortawesome/free-solid-svg-icons/faShoppingCart';
-import {useRouter} from 'next/router';
+import { ICartItem } from "boundless-api-client";
+import {
+	Dispatch,
+	SetStateAction,
+	useEffect,
+	useMemo,
+	useRef,
+	useState,
+} from "react";
+import { useAppDispatch, useAppSelector } from "../../hooks/redux";
+import { apiClient } from "../../lib/api";
+import { addPromise } from "../../redux/reducers/xhr";
+import { RootState } from "../../redux/store";
+import debounce from "lodash/debounce";
+import CartRow from "./CartRow";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons/faShoppingCart";
+import { useRouter } from "next/router";
+import { formatMoney } from "../../lib/formatter";
 
-export default function CartItems({items, setItems, total}: ICartItemsProps) {
+export default function CartItems({ items, setItems, total }: ICartItemsProps) {
 	const dispatch = useAppDispatch();
 	const submits = useRef<Promise<any>[]>([]);
 	const mounted = useRef(false);
@@ -23,51 +31,57 @@ export default function CartItems({items, setItems, total}: ICartItemsProps) {
 		if (!size || !mounted.current) return;
 
 		setSubmitting(true);
-		Promise.allSettled(submits.current)
-			.then(() => {
-				if (submits.current.length === size) {
-					setSubmitting(false);
-					submits.current = [];
-				}
-			});
+		Promise.allSettled(submits.current).then(() => {
+			if (submits.current.length === size) {
+				setSubmitting(false);
+				submits.current = [];
+			}
+		});
 	};
 
 	const rmItem = (itemId: number) => {
 		if (!cartId) return;
-		if (!confirm('Are you sure?')) return;
+		if (!confirm("Are you sure?")) return;
 
 		setSubmitting(true);
-		const promise = apiClient.cart.removeFromCart(cartId, [itemId])
-		.then(() => checkBgSubmits());
+		const promise = apiClient.cart
+			.removeFromCart(cartId, [itemId])
+			.then(() => checkBgSubmits());
 
 		submits.current.push(promise);
 		dispatch(addPromise(promise));
-		setItems(prevItems => prevItems.filter(el => el.item_id !== itemId));
+		setItems((prevItems) => prevItems.filter((el) => el.item_id !== itemId));
 	};
 
 	const submitQty = async (itemId: number, newQty: number) => {
 		if (!cartId) return;
 
-		const promise = apiClient.cart.setCartItemsQty(cartId, [{
-			item_id: itemId,
-			qty: newQty
-		}])
+		const promise = apiClient.cart
+			.setCartItemsQty(cartId, [
+				{
+					item_id: itemId,
+					qty: newQty,
+				},
+			])
 			.then(() => checkBgSubmits());
 		submits.current.push(promise);
 	};
 
-	const debouncedSubmitQty = useMemo(() =>
-		debounce(
-			(itemId: number, qty: number) => submitQty(itemId, qty), 700, {leading: true}
-		), []);// eslint-disable-line
+	const debouncedSubmitQty = useMemo(
+		() =>
+			debounce((itemId: number, qty: number) => submitQty(itemId, qty), 700, {
+				leading: true,
+			}),
+		[]
+	); // eslint-disable-line
 
 	const onQtyChange = (itemId: number, newQty: number) => {
 		setSubmitting(true);
 		debouncedSubmitQty(itemId, newQty);
 
-		setItems(prevFiltered => {
+		setItems((prevFiltered) => {
 			const out = [...prevFiltered];
-			const index = out.findIndex(el => el.item_id === itemId);
+			const index = out.findIndex((el) => el.item_id === itemId);
 			if (index >= 0) {
 				out[index].qty = newQty;
 			}
@@ -84,38 +98,41 @@ export default function CartItems({items, setItems, total}: ICartItemsProps) {
 
 	return (
 		<>
-			<div className='cart-items'>
-				<div className='cart-items__thead row'>
-					<div className='cart-items__thead-cell col-md-4'></div>
-					<div className='cart-items__thead-cell col-md-2'>Price</div>
-					<div className='cart-items__thead-cell col-md-2'>Qty</div>
-					<div className='cart-items__thead-cell col-md-2'>Total</div>
-					<div className='cart-items__thead-cell col-md-2'></div>
+			<div className="cart-items">
+				<div className="cart-items__thead row">
+					<div className="cart-items__thead-cell col-md-4"></div>
+					<div className="cart-items__thead-cell col-md-2">Price</div>
+					<div className="cart-items__thead-cell col-md-2">Qty</div>
+					<div className="cart-items__thead-cell col-md-2">Total</div>
+					<div className="cart-items__thead-cell col-md-2"></div>
 				</div>
-				{items.map(item => (
+				{items.map((item) => (
 					<CartRow
 						item={item}
-						rmItem={() => rmItem(item.item_id)} key={item.item_id}
+						rmItem={() => rmItem(item.item_id)}
+						key={item.item_id}
 						onQtyChange={(qty: number) => onQtyChange(item.item_id, qty)}
 					/>
 				))}
-				<div className='cart-items__total-row row'>
-					<div className='cart-items__total-cell cart-items__total-cell_title col-md-6'>Order Total:</div>
-					<div className='cart-items__total-cell col-md-2'>
-						<span className='cart-items__label'>Qty: </span>
+				<div className="cart-items__total-row row">
+					<div className="cart-items__total-cell cart-items__total-cell_title col-md-6">
+						Order Total:
+					</div>
+					<div className="cart-items__total-cell col-md-2">
+						<span className="cart-items__label">Qty: </span>
 						{total.qty}
 					</div>
-					<div className='cart-items__total-cell col-md-2'>
-						<span className='cart-items__label'>Price: </span>
-						{total.price}
+					<div className="cart-items__total-cell col-md-2">
+						<span className="cart-items__label">Price: </span>
+						{formatMoney(total.price)}
 					</div>
 				</div>
 			</div>
-			<div className='cart-items__actions'>
+			<div className="cart-items__actions">
 				<button
-					className='btn btn-action btn-lg btn-anim'
+					className="btn btn-action btn-lg btn-anim"
 					disabled={submitting}
-					onClick={() => router.push('/checkout')}
+					onClick={() => router.push("/checkout")}
 				>
 					Proceed to checkout <FontAwesomeIcon icon={faShoppingCart} />
 				</button>
@@ -127,5 +144,5 @@ export default function CartItems({items, setItems, total}: ICartItemsProps) {
 interface ICartItemsProps {
 	items: ICartItem[];
 	setItems: Dispatch<SetStateAction<ICartItem[]>>;
-	total: {qty: number, price: string}
+	total: { qty: number; price: string };
 }
