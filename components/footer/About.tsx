6a468e2af02cbfdbb37957b741fcb382a21cba8c
Vedant Nimbarte <nimbartevedant@gmail.com>
2023-02-08 22:39:54 +0530

    Company name

--- components/footer/About.tsx
+++ components/footer/About.tsx
@@ -1,8 +1,12 @@
 // import Link from 'next/link';
 // import logoImg from '../../assets/logo.svg';
 
-export default function FooterAbout({companyTitle}: {companyTitle?: string}) {
-	const title = companyTitle || '© Your Company LLC.';
+export default function FooterAbout({
+	companyTitle,
+}: {
+	companyTitle?: string;
+}) {
+	const title = companyTitle || "© SizePlus Apparel.pvt.ltd";
 	return (
 		<>
 			{/* <div className='page-footer__logo'>
@@ -12,12 +16,14 @@ export default function FooterAbout({companyTitle}: {companyTitle?: string}) {
 					</a>
 				</Link>
 			</div> */}
-			<div className='page-footer__company-info'>
-				<p className='title'>{title}</p>
+			<div className="page-footer__company-info">
+				<p className="title">{title}</p>
 			</div>
-			<div className='page-footer__disclaimer'>
-					This website and its contents are provided "as is" and "as available" without any warranty or representations of any kind, whether express or implied.
-					Price and availability information is subject to change without notice.
+			<div className="page-footer__disclaimer">
+				This website and its contents are provided "as is" and "as available"
+				without any warranty or representations of any kind, whether express or
+				implied. Price and availability information is subject to change without
+				notice.
 			</div>
 		</>
 	);
