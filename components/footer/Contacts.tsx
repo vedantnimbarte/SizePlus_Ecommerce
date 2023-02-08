3b152903669b7313d2cb96edaa5cdf64fec2e4ab
Vedant Nimbarte <nimbartevedant@gmail.com>
2023-02-08 22:40:07 +0530

    Updated contact details

--- components/footer/Contacts.tsx
+++ components/footer/Contacts.tsx
@@ -1,27 +1,32 @@
-import {faWhatsapp} from '@fortawesome/free-brands-svg-icons/faWhatsapp';
-import {faClock} from '@fortawesome/free-solid-svg-icons/faClock';
-import {faMapMarkerAlt} from '@fortawesome/free-solid-svg-icons/faMapMarkerAlt';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons/faWhatsapp";
+import { faClock } from "@fortawesome/free-solid-svg-icons/faClock";
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons/faMapMarkerAlt";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function FooterContacts() {
 	return (
 		<>
-			<h3 className='page-footer__header'>Contact Us</h3>
-			<p className='page-footer__icon-w-link'>
-				<span className='icon'>
-					<FontAwesomeIcon icon={faWhatsapp}/>
+			<h3 className="page-footer__header">Contact Us</h3>
+			<p className="page-footer__icon-w-link">
+				<span className="icon">
+					<FontAwesomeIcon icon={faWhatsapp} />
 				</span>
-				<a className='link' href='tel:+18001234567'>+1 (800) 123-45-67</a>
+				<a className="link" href="tel:+919836669720">
+					+91 983 666 9720
+				</a>
 			</p>
-			<p className='page-footer__icon-w-link'>
-				<span className='icon'>
-					<FontAwesomeIcon icon={faMapMarkerAlt}/>
+			<p className="page-footer__icon-w-link">
+				<span className="icon">
+					<FontAwesomeIcon icon={faMapMarkerAlt} />
 				</span>
-				<a className='link' href='#'>1 infinite loop, Cupertino, CA 95014</a>
+				<a className="link" href="#">
+					Diamond Arcade, Room No 401/407, 4 th Floor, 68 Jessore
+					Road, Kolkata-700055
+				</a>
 			</p>
-			<p className='page-footer__icon-w-link'>
-				<span className='icon'>
-					<FontAwesomeIcon icon={faClock}/>
+			<p className="page-footer__icon-w-link">
+				<span className="icon">
+					<FontAwesomeIcon icon={faClock} />
 				</span>
 				9:00am &mdash; 6:00pm
 			</p>
