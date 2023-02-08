750d625c260f0f94f26a368c4e058ee690e21a47
Vedant Nimbarte <nimbartevedant@gmail.com>
2023-02-08 22:43:05 +0530

    Updated contact details

--- components/header/MarsTopNav.tsx
+++ components/header/MarsTopNav.tsx
@@ -1,39 +1,45 @@
-import {ReactNode} from 'react';
-import {faWhatsapp} from '@fortawesome/free-brands-svg-icons/faWhatsapp';
-import {faPhoneAlt} from '@fortawesome/free-solid-svg-icons/faPhoneAlt';
-import {faShippingFast} from '@fortawesome/free-solid-svg-icons/faShippingFast';
+import { ReactNode } from "react";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons/faWhatsapp";
+import { faPhoneAlt } from "@fortawesome/free-solid-svg-icons/faPhoneAlt";
+import { faShippingFast } from "@fortawesome/free-solid-svg-icons/faShippingFast";
 
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import Link from 'next/link';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 export default function MarsTopNav() {
 	return (
-		<nav className={'mars-top-nav'}>
-			<div className={'container-xxl mars-top-nav__container'}>
-				<div className={'mars-top-nav__contacts'}>
-					<Contact href={'tel:+18001234567'}
-									 label={'+1 (800) 123-45-67'}
-									 icon={<FontAwesomeIcon icon={faPhoneAlt} />}
+		<nav className={"mars-top-nav"}>
+			<div className={"container-xxl mars-top-nav__container"}>
+				<div className={"mars-top-nav__contacts"}>
+					<Contact
+						href={"tel:++91 983 666 9720"}
+						label={"+91 983 666 9720"}
+						icon={<FontAwesomeIcon icon={faPhoneAlt} />}
 					/>
-					<Contact href={'https://api.whatsapp.com/send?phone=10001234567&text=hello'}
-									 label={'+1 (000) 123-45-67'}
-									 icon={<FontAwesomeIcon icon={faWhatsapp} />}
+					<Contact
+						href={"https://api.whatsapp.com/send?phone=919836669720&text=hello"}
+						label={"+91 983 666 9720"}
+						icon={<FontAwesomeIcon icon={faWhatsapp} />}
 					/>
 				</div>
-				<div className={'mars-top-nav__note'}>
+				<div className={"mars-top-nav__note"}>
 					<p>
-						Opening hours 9 a.m. - 6 p.m. <FontAwesomeIcon icon={faShippingFast} className={'text-theme-color ms-1'} />
+						Opening hours 9 a.m. - 6 p.m.{" "}
+						<FontAwesomeIcon
+							icon={faShippingFast}
+							className={"text-theme-color ms-1"}
+						/>
 					</p>
 				</div>
-				<ul className={'mars-top-nav__menu list-unstyled'}>
-					<li className={'mars-top-nav__menu-item'}>
-						<Link href={'/shipping'}>
-							<a className='mars-top-nav__menu-link'>Shipping</a>
+				<ul className={"mars-top-nav__menu list-unstyled"}>
+					<li className={"mars-top-nav__menu-item"}>
+						<Link href={"/shipping"}>
+							<a className="mars-top-nav__menu-link">Shipping</a>
 						</Link>
 					</li>
-					<li className={'mars-top-nav__menu-item'}>
-						<Link href={'/about'}>
-							<a className='mars-top-nav__menu-link'>About</a>
+					<li className={"mars-top-nav__menu-item"}>
+						<Link href={"/about"}>
+							<a className="mars-top-nav__menu-link">About</a>
 						</Link>
 					</li>
 				</ul>
@@ -42,12 +48,23 @@ export default function MarsTopNav() {
 	);
 }
 
-const Contact = ({href, label, icon}: {href: string, label: string, icon?: ReactNode}) => {
+const Contact = ({
+	href,
+	label,
+	icon,
+}: {
+	href: string;
+	label: string;
+	icon?: ReactNode;
+}) => {
 	return (
-		<div className={'mars-top-nav__contact'}>
-			{icon &&
-			<a className={'mars-top-nav__contact-icon'} href={href}>{icon}</a>}
-			<a className={'mars-top-nav__contact-txt'} href={href}>
+		<div className={"mars-top-nav__contact"}>
+			{icon && (
+				<a className={"mars-top-nav__contact-icon"} href={href}>
+					{icon}
+				</a>
+			)}
+			<a className={"mars-top-nav__contact-txt"} href={href}>
 				{label}
 			</a>
 		</div>
